2e1b2d6c0954fdcac4c0421a8c19103347869e52
HuchMan <ucha19871@gmail.com>
2016-10-15 00:28:44 +0400

    -amundu previous changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component} from "@angular/core";
-import {Location} from "@angular/common"
-import {Router} from "@angular/router";
-import * as AppLocationSettings from "application-settings";
 
 @Component({
     selector: "my-app",
@@ -9,8 +6,6 @@ import * as AppLocationSettings from "application-settings";
 })
 export class AppComponent {
     public counter: number = 16;
-    public personList: Array<Object>
-    
     public get message(): string {
         if (this.counter > 0) {
             return this.counter + " taps left";
@@ -18,17 +13,7 @@ export class AppComponent {
             return "Hoorraaay! \nYou are ready to start building!";
         }
     }
-    
     public onTap() {
         this.counter--;
     }
-    
-    
-    constructor(private router:Router,location:Location){
-        this.personList = JSON.parse(AppLocationSettings.getString("people","[]"));
-        location.subscribe(path => {
-            this.personList = JSON.parse(AppLocationSettings.getString("people","[]"));
-        });
-    }
-    
 }
