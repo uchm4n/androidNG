c05838fd81e001f8b8bfc3b194cd69b4bc41f35e
ucha <ucha@clips4sale.com>
2016-10-25 10:55:30 +0400

    Norris taps

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
-import { NgModule } from "@angular/core";
+import {NgModule, enableProdMode} from "@angular/core";
 import { AppComponent } from "./app.component";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
@@ -16,4 +16,5 @@ import {FormsModule} from "@angular/forms";
 })
 class AppComponentModule {}
 
+enableProdMode();
 platformNativeScriptDynamic().bootstrapModule(AppComponentModule);
